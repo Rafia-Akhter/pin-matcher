8e96e8a5351d1c302dc3fc88d37a05bfc342742d
unknown <rafu.baec@gmail.com>
2020-08-01 13:36:31 +0800

    some add

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 
 
+ 
+ var fourDigitRandomNum =Math.floor(1000+ Math.random()*9000);
+ var output = Math.floor(fourDigitRandomNum);
+console.log(output);
 
 
- const displayBtn = document.getElementById('display');
- displayBtn.addEventListener("click",function(){  document.body.style.backgroundColor = 'cyan';
-
- })
-
- var num = 1;
- var randomNum = Math.random()* 4;
- var output = Math.floor(randomNum);
- console.log(output);
 
 
+ 
