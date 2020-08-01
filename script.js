76d299d9e0daa3e9cdd449fa107ffd966b554e08
unknown <rafu.baec@gmail.com>
2020-08-01 10:17:35 +0800

    sum add

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-  function insert(num) {
-    document.forms.text.value =  document.forms.text.value + num;
-    
-  }
-  function clear() {
-    document.forms.text.value = '';
-  }
-  function back() {
-    var exp = document.forms.text.value;
-    document.forms.text.value = exp.substring(0,exp.length-1);
-  }
+
+
+
+
+ const displayBtn = document.getElementById('display');
+ displayBtn.addEventListener("click",function(){  document.body.style.backgroundColor = 'cyan';
+
+ })
+
+ var num = 1;
+ var randomNum = Math.random()* 4;
+ var output = Math.floor(randomNum);
+ console.log(output);
 
 
