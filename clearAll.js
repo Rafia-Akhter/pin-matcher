063dde0c07029ca6e9e69728d979d7281d742dcf
unknown <rafu.baec@gmail.com>
2020-08-04 20:58:39 +0800

    finished

---
+++ clearAll.js
@@ -0,0 +1,3 @@
+function clearAll() {
+   document.querySelector(".inputValueShower").value = null;
+}
--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-document.querySelector(".notify-section").getElementsByClassName.display = "none";
+ document.querySelector(".notify-section").style.display = "none";
+ const buttons = document.querySelectorAll("input[type=button]");
+const length = buttons.length;
+for (let i = 0; i < length; i++) {
+  buttons[i].addEventListener("click", handle);
+}
+  function handle(event){
+     const value = event.target.value;
+  }
 
-document.querySelector(".generate-btm").addEventListener("click", generatePin);
+document.querySelector(".generate-btn").addEventListener("click", generatePin);
 
 function generatePin(){
-   document.querySelector("form-control").value = Math.ceil(Math.random()*(9999-1000 + 1))+1000 ;
+   document.querySelector("form-control").value = Math.floor(Math.random(9999-1000))+1000 ;
 
    document.querySelector(".form-control").style.color = "white";
+   document.querySelector(".generatedValue").style.value = "center";
+   document.querySelector(".generatedValue").style.textAlign = "2em";
 
 }
 
 function inputValueTaker(x){
-      const inputAmount = document.querySelector(".inputValueShower")
-      inputAmount.value = inputAmount.value + x
+      const inputShower = document.querySelector(".inputValueShower")
+      inputShower.value = inputShower.value + x;
       document.querySelector(".inputValueShower").style.color = "white";
       document.querySelector(".inputValueShower").style.textAlign ="right";
+      inputValueShower.style.textAlign = "right";
 
    }
 
@@ -23,18 +34,35 @@ function inputValueTaker(x){
 
 
     if (assignValue == generatedValue) {
-       document.querySelector(".notify-section").style.display = "block";
-       document.querySelector(".wrong").style.display = "none";
+       document.querySelector(".notify-section").style.display.value = "block";
+       document.querySelector(".right").style.display.value = "block";
+       document.querySelector(".wrong").style.display.value = "none";
+       document.querySelector(".action-left").style.display.value = "none";
+       document.querySelector(".submit-btn").disabled.value =" true";
      }
-     else {
-       document.querySelector(".notify-section").style.display = "block";
-       document.querySelector(".right").style.display = "none";
+     else  {
+       document.querySelector(".notify-section").style.display.value = "block";
+       document.querySelector(".wrong").style.display = "block";
+       document.querySelector(".right").style.display= "none";
+       document.querySelector(".action-left").style.display = "block";
+
 
 
      } 
-
+   const tryLeft = document.querySelector("#tryLeft").innerText;
+   let newTryLeft = parseInt(tryLeft);
+   if(newTryLeft > 0){
+      newTryLeft = newTryLeft - 1;
 
    }
+   document.querySelector("#tryLeft").innerText = newTryLeft;
+
+   if(newTryLeft == 0){
+      document.querySelector(".submit-btn").disabled = true;
+
+   }
+
+   
    document.querySelector(".inputValueShower").value = '';
    document.querySelector(".action-left").innerText = 1+ 'try left'
 
@@ -54,4 +82,11 @@ function clearAll(){
 
 
 
+   }
+   function removeFromLast(){
+     const currentValue = document.querySelector(".inputValueShower").value;
+     const newCurrentValue = currentValue.substring(0, currentValue.length - 1);
+     document.querySelector(".inputValueShower").value = newCurrentValue;
+
+   }
 
